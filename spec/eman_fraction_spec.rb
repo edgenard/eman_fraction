6a5673b6754f1b2f020233dff07a51bbe7fca989
Emamnuel <emmanuel@teepublic.com>
2020-11-08 14:00:18 -0500

    Test against new instances of Fraction

--- spec/eman_fraction_spec.rb
+++ spec/eman_fraction_spec.rb
@@ -6,15 +6,16 @@ RSpec.describe Fraction do
   describe '+' do
     it 'zero plus zero equals zero' do
       zero = Fraction.new(0)
-      expect(zero + zero).to eq(0)
+      expect(zero + zero).to eq(Fraction.new(0).number)
     end
 
     it 'zero plus three equals three' do
       zero = Fraction.new(0)
       three = Fraction.new(3)
 
-      expect(zero + three).to eq(3)
+      expect(zero + three).to eq(Fraction.new(3).number)
     end
+
   end
 
 end
