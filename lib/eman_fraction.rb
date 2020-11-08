a8bcfc037e12eb7f679111426763e484a7683c25
Emamnuel <emmanuel@teepublic.com>
2020-11-08 14:53:40 -0500

    Pulled back to simpler test to reduce fractions on creation

--- lib/eman_fraction.rb
+++ lib/eman_fraction.rb
@@ -2,19 +2,20 @@ class Fraction
   attr_reader :numerator, :denominator
 
   def initialize(numerator, denominator = 1)
-    @numerator = numerator
-    @denominator = denominator
+    gcd = numerator.gcd(denominator)
+    @numerator = numerator / gcd
+    @denominator = denominator / gcd
   end
 
   def +(other)
     if denominator >= other.denominator
-      smallest_common_multiple =  denominator / other.denominator
-      numerator_sum = numerator + (other.numerator * smallest_common_multiple)
       common_denominator = denominator
+      smallest_common_multiple =  common_denominator / other.denominator
+      numerator_sum = numerator + (other.numerator * smallest_common_multiple)
     else
-      smallest_common_multiple = other.denominator / denominator
-      numerator_sum = (numerator * smallest_common_multiple) + other.numerator
       common_denominator = other.denominator
+      smallest_common_multiple = common_denominator / denominator
+      numerator_sum = (numerator * smallest_common_multiple) + other.numerator
     end
 
     Fraction.new(numerator_sum, common_denominator)
