c03a40f1861ca3b1c1df7f03e1904ae271ecf90e
Emamnuel <emmanuel@teepublic.com>
2020-11-08 13:56:15 -0500

    Addition with zero as either operand

--- lib/eman_fraction.rb
+++ lib/eman_fraction.rb
@@ -1,63 +1,10 @@
-class Fraction < Numeric
-  attr_reader :numerator, :denominator
-  def initialize(numerator:, denominator:)
-    raise InvalidFraction, 'denominator cannot be zero' if denominator.zero?
-
-    @numerator = numerator
-    @denominator = denominator
-    simplify
+class Fraction
+  attr_reader :number
+  def initialize(number)
+    @number = number
   end
 
   def +(other)
-    return 0 if zeros?(other) # Don't really know how to deal with this otherwise
-
-    self_numerator = numerator * other.denominator
-    other_numerator = other.numerator * denominator
-    new_fraction = self.class.new(numerator: self_numerator + other_numerator, denominator: other.denominator * denominator)
-
-    return 0 if new_fraction.numerator.zero?
-
-    new_fraction
-  end
-
-  def -(other)
-    self_numerator = numerator * other.denominator
-    other_numerator = other.numerator * denominator
-    new_fraction = self.class.new(
-      numerator: self_numerator - other_numerator,
-      denominator: other.denominator * denominator
-    )
-    return 0 if new_fraction.numerator.zero?
-
-    new_fraction
-  end
-
-  def ==(other)
-    numerator == other.numerator && denominator == other.denominator
-  end
-
-  def coerce(other)
-    [self.class.new(numerator: other.to_i, denominator: 1), self]
-  end
-
-  def simplify
-    denon_factors = factors(denominator)
-    num_factors = factors(numerator)
-    common_factors = denon_factors & num_factors
-    return if common_factors.empty?
-
-    @numerator /= common_factors.last
-    @denominator /= common_factors.last
-  end
-
-  def factors(number)
-    (1..number).select { |x| number % x == 0 }
-  end
-
-  def zeros?(other)
-    (other.zero? || (other.numerator.zero? && other.denominator.zero?)) &&
-      (numerator.zero? && denominator.zero?)
-  end
-  class InvalidFraction < StandardError
+    @number + other.number
   end
 end
--- spec/eman_fraction_spec.rb
+++ spec/eman_fraction_spec.rb
@@ -2,103 +2,19 @@ require 'spec_helper'
 require_relative '../lib/eman_fraction'
 
 RSpec.describe Fraction do
-  describe '.new' do
-    it 'throws an error if denominator is zero' do
-      expect { Fraction.new(numerator: 1, denominator: 0) }.to raise_error(Fraction::InvalidFraction)
-    end
-  end
+
   describe '+' do
-    it 'adds proper and improper fractions' do
-      four_fifths = Fraction.new(numerator: 4, denominator: 5)
-      seven_thirds = Fraction.new(numerator: 7, denominator: 3)
-
-      expect(four_fifths + seven_thirds).to eq Fraction.new(numerator: 47, denominator: 15)
+    it 'zero plus zero equals zero' do
+      zero = Fraction.new(0)
+      expect(zero + zero).to eq(0)
     end
 
-    it 'adds two proper fractions' do
-      two_thirds = Fraction.new(numerator: 2, denominator: 3)
-      three_fourths = Fraction.new(numerator: 3, denominator: 4)
+    it 'zero plus three equals three' do
+      zero = Fraction.new(0)
+      three = Fraction.new(3)
 
-      expect(two_thirds + three_fourths).to eq Fraction.new(numerator: 17, denominator: 12)
-    end
-
-    it 'works with integers' do
-      five_sevenths = Fraction.new(numerator: 5, denominator: 7)
-
-      expect(five_sevenths + 5).to eq Fraction.new(numerator: 40, denominator: 7)
-    end
-
-    it 'represents fractions in the lowest terms' do
-      five_ninths = Fraction.new(numerator: 5, denominator: 9)
-      eleven_twefths = Fraction.new(numerator: 11, denominator: 12)
-
-      expect(five_ninths + eleven_twefths).to eq Fraction.new(numerator: 53, denominator: 36)
-    end
-
-    it 'can add zeros to fractions' do
-      one_third = Fraction.new(numerator: 1, denominator: 3)
-
-      expect(one_third + 0).to eq Fraction.new(numerator: 1, denominator: 3)
-    end
-
-    it 'returns whole numbers as integers' do
-      one_half = Fraction.new(numerator: 5, denominator: 10)
-
-      expect(one_half + one_half).to eq(1)
-    end
-
-    it 'returns zero when negating a fraction' do
-      one_third = Fraction.new(numerator: 1, denominator: 3)
-      negative_one_third = Fraction.new(numerator: -1, denominator: 3)
-
-      expect(one_third + negative_one_third).to eq 0
+      expect(zero + three).to eq(3)
     end
   end
 
-  describe '-' do
-    it 'does simple subtraction' do
-      one_third = Fraction.new(numerator: 1, denominator: 3)
-      two_thirds = Fraction.new(numerator: 2, denominator: 3)
-
-      expect(two_thirds - one_third).to eq Fraction.new(numerator: 1, denominator: 3)
-    end
-
-    it 'subtracts fractions with different denominators' do
-      five_sevenths = Fraction.new(numerator: 5, denominator: 7)
-      one_third = Fraction.new(numerator: 1, denominator: 3)
-
-      expect(five_sevenths - one_third).to eq Fraction.new(numerator: 8, denominator: 21)
-    end
-
-    it 'subtract fractions that result in negative values' do
-      five_sevenths = Fraction.new(numerator: 5, denominator: 7)
-      one_third = Fraction.new(numerator: 1, denominator: 3)
-
-      expect(one_third - five_sevenths).to eq Fraction.new(numerator: -8, denominator: 21)
-    end
-
-    it 'subtracting the same fraction results in zero' do
-      one_third = Fraction.new(numerator: 1, denominator: 3)
-
-      expect(one_third - one_third).to eq 0
-    end
-
-    it 'returns the fraction when subtracting zero' do
-      one_third = Fraction.new(numerator: 1, denominator: 3)
-
-      expect(one_third - 0).to eq Fraction.new(numerator: 1, denominator: 3)
-    end
-
-    it 'negates the fraction when subtracting from zero' do
-      one_third = Fraction.new(numerator: 1, denominator: 3)
-
-      expect(0 - one_third).to eq Fraction.new(numerator: -1, denominator: 3)
-    end
-
-    it 'subtracts from integers' do
-      one_third = Fraction.new(numerator: 1, denominator: 3)
-
-      expect(5 - one_third).to eq Fraction.new(numerator: 14, denominator: 3)
-    end
-  end
 end
