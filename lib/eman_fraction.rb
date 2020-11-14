049d1dc0c407d1abaa1bc5b05ac532572e246d53
Emamnuel <emmanuel@teepublic.com>
2020-11-14 10:58:29 -0500

    adds simple fractions

--- lib/eman_fraction.rb
+++ lib/eman_fraction.rb
@@ -1,10 +1,16 @@
 class Fraction
-  attr_reader :numerator
-  def initialize(numerator)
+  attr_reader :numerator, :denominator
+  def initialize(numerator, denominator = 1)
     @numerator = numerator
+    @denominator = denominator
   end
 
   def +(other)
-    numerator + other.numerator
+    self.class.new(numerator + other.numerator, denominator)
+  end
+
+
+  def ==(other)
+    other.numerator == numerator
   end
 end
--- spec/eman_fraction_spec.rb
+++ spec/eman_fraction_spec.rb
@@ -10,5 +10,13 @@ RSpec.describe Fraction do
 
       expect(four + nine).to eq 13
     end
+
+    it 'adds simple fractions' do
+      one_fifth = Fraction.new(1, 5)
+      two_fifths = Fraction.new(2, 5)
+      three_fifths = Fraction.new(3, 5)
+
+      expect(one_fifth + two_fifths).to eq three_fifths
+    end
   end
 end
