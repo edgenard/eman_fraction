c09c66ce43f50123fd45c5d21a57c6798c857169
Emamnuel <emmanuel@teepublic.com>
2020-11-14 10:12:08 -0500

    Remove unnecessary tests. Confirming that it adds integers is enough

--- spec/eman_fraction_spec.rb
+++ spec/eman_fraction_spec.rb
@@ -4,19 +4,7 @@ require_relative '../lib/eman_fraction'
 RSpec.describe Fraction do
 
   describe '+' do
-    it 'adds zeros' do
-      zero = Fraction.new(0)
-      expect(zero + zero).to eq 0
-    end
-
-    it 'adds zero to integers' do
-      zero = Fraction.new(0)
-      one = Fraction.new(1)
-
-      expect(one + zero).to eq 1
-    end
-
-    it 'adds non-zero integers' do
+    it 'adds integers' do
       four = Fraction.new(4)
       nine = Fraction.new(9)
 
